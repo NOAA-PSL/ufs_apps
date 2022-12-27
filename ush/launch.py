95a83b95374f3f047f65fe36bc6f4c506356d0e1
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2022-12-26 17:41:40 -0700

    Launch application module.

---
+++ ush/launch.py
@@ -0,0 +1,52 @@
+# =========================================================================
+
+# Module: ush/launch.py
+
+# Author: Henry R. Winterbottom
+
+# Email: henry.winterbottom@noaa.gov
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the respective public license published by the
+# Free Software Foundation and included with the repository within
+# which this application is contained.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+# =========================================================================
+
+"""
+
+"""
+
+# ----
+
+from tools import fileio_interface
+from tools import parser_interface
+
+# ----
+
+
+class Launch():
+    """
+
+    """
+
+    def __init__(self, options_obj: object):
+        """
+        Description
+        -----------
+
+        Creates a new Launch object.
+
+        """
+
+    def run(self) -> None:
+        """
+
+
+        """
+
+        # Define the experiment configuration.
