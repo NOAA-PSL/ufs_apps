96f90025aebfa0278b98d69ce2204a6fe5b6a292
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2022-12-26 17:53:55 -0700

    Developing and debugging.

--- ush/launch.py
+++ ush/launch.py
@@ -25,16 +25,19 @@
 
 from tools import fileio_interface
 from tools import parser_interface
+from utils.error_interface import Error
+from utils.logger_interface import Logger
 
 # ----
 
 
-class Launch():
+class Launch:
     """
 
     """
 
-    def __init__(self, options_obj: object):
+    @classmethod
+    def __init__(cls, options_obj: object):
         """
         Description
         -----------
@@ -43,6 +46,43 @@ class Launch():
 
         """
 
+        # Define the base-class attributes.
+        self = cls
+        self.options_obj = options_obj
+        self.logger = Logger()
+
+        # Check that the mandatory arguments have been provided within
+        # the options_obj parameter; proceed accordingly.
+        mand_args_list = ["cycle", "expt_name", "work_path", "yaml_file"]
+
+        for mand_arg in mand_args_list:
+
+            # Check that the mandatory argument has been provided;
+            # proceed accordingly.
+            value = parser_interface.object_getattr(
+                object_in=self.options_obj, key=mand_arg, force=True
+            )
+
+            if value is None:
+                msg = (
+                    "The option attributes provided to the base-class does not "
+                    f"contain the mandatory attribute {mand_arg}. Aborting!!!"
+                )
+                raise LaunchError(msg=msg)
+
+            # Update the base-class object attribute.
+            self = parser_interface.object_setattr(
+                object_in=self, key=mand_arg, value=value
+            )
+
+        # Check that the timestamp string is of the correct format;
+        # proceed accordingly.
+        timestamp_interface.check_frmt(
+            datestr=self.cycle,
+            in_frmttyp=timestamp_interface.GLOBAL,
+            out_frmttyp=timestamp_interface.GLOBAL,
+        )
+
     def run(self) -> None:
         """
 
@@ -50,3 +90,33 @@ class Launch():
         """
 
         # Define the experiment configuration.
+
+# ----
+
+
+class LaunchError(Error):
+    """
+    Description
+    -----------
+
+    This is the base-class for all exceptions; it is a sub-class of
+    Error.
+
+    Parameters
+    ----------
+
+    msg: str
+
+        A Python string to accompany the raised exception.
+
+    """
+
+    def __init__(self, msg: str):
+        """
+        Description
+        -----------
+
+        Creates a new LaunchError object.
+
+        """
+        super().__init__(msg=msg)
