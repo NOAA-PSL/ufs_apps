604701a064d677719faf263ed479b5f326d44f1a
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-01-19 19:31:07 -0700

    Updates for new argument module.

--- scripts/exufs_fetch.py
+++ scripts/exufs_fetch.py
@@ -44,51 +44,8 @@ Usage
     user@host:$ python exufs_fetch.py --<yaml_file> --<cycle> --<work_path> \
                     --<expt_name> [--platform] [--fetch_type] [--fileid]
 
-Author(s)
----------
-
-    Henry R. Winterbottom; 12 December 2022
-
-History
--------
-
-    2022-12-12: Henry Winterbottom -- Initial implementation.
-
-"""
-
-# ----
-
-# pylint: disable=no-name-in-module
-
-# ----
-
-import os
-import time
-
-from schema import Optional, Or
-from staging.fetch import Fetch
-from utils.arguments_interface import Arguments
-from utils.logger_interface import Logger
-
-# ----
-
-__author__ = "Henry R. Winterbottom"
-__maintainer__ = "Henry R. Winterbottom"
-__email__ = "henry.winterbottom@noaa.gov"
-
-# ----
-
-
-def main() -> None:
-    """
-    Description
-    -----------
-
-    This is the driver-level function to invoke the tasks within this
-    script.
-
-    Parameters
-    ----------
+Parameters
+----------
 
     yaml_file: str
 
@@ -105,7 +62,7 @@ def main() -> None:
 
         Enter the parameter value as:
 
-        --yaml_file=/path/to/yaml/file or -yaml_file=/path/to/yaml/file
+        --yaml_file /path/to/yaml/file or -yaml_file /path/to/yaml/file
 
     cycle: str
 
@@ -114,18 +71,23 @@ def main() -> None:
         convention; enter the parameter value as follows for a
         forecast cycle beginning 0000 UTC 01 January 2000:
 
-        --cycle=20000101000000 or -cycle=20000101000000
+        --cycle 20000101000000 or -cycle 20000101000000
 
     expt_name: str
 
         A Python string specifying an (unique) name for the respective
-        experiment.
+        experiment; for an experiment named SPAM, enter the parameter
+        value as follows.
+
+        --expt_name SPAM or -expt_name SPAM
 
     work_path: str
 
         A Python string specifying the path to where the experiment
         directory trees will be built and the respective experiment
-        will be executed.
+        will be executed; enter the parameter value as follows.
+
+        --work_path /path/for/spam/experiment or -work_path /path/for/spam/experiment
 
     Keywords
     --------
@@ -138,15 +100,15 @@ def main() -> None:
         interface/platform options (no spaces between comma-delimited
         values).
 
-        For files to be fetched from the 'spam' platform, the keyword
+        For files to be fetched from the "spam" platform, the keyword
         value may be entered as:
 
-        --platform=spam or -platform=spam
+        --platform spam or -platform spam
 
-        For files to be fetched from both the 'spam' and the 'ham'
+        For files to be fetched from both the "spam" and the "ham"
         platforms, the keyword value may be entered as:
 
-        --platform=spam,ham or -platform=spam,ham
+        --platform spam,ham or -platform spam,ham
 
     fetch_type: str, optional
 
@@ -161,16 +123,16 @@ def main() -> None:
 
                 ham:
 
-        if fetch_type is specified as 'spam', all files for the
+        if fetch_type is specified as "spam", all files for the
         respective interface (i.e., AWS s3, NOAA HPSS, etc.,) beneath
-        the 'spam' block will be collected; if the keyword is not
+        the "spam" block will be collected; if the keyword is not
         specified or NoneType upon entry, the attributes beneath both
-        'spam' and 'ham' will be returned.
+        "spam" and "ham" will be returned.
 
-        For the 'spam' example above, the keyword value may be entered
+        For the "spam" example above, the keyword value may be entered
         as:
 
-        --fetch_type=spam or -fetch_type=spam
+        --fetch_type spam or -fetch_type spam
 
     fileid: str, optional
 
@@ -192,39 +154,85 @@ def main() -> None:
 
                     eggs:
 
-        The 'ham' and 'eggs' attributes are the file identifiers
+        The "ham" and "eggs" attributes are the file identifiers
         within the configuration file and may be used to retrieve
-        specific (a) file(s). For a respective file identifier, 'ham'
+        specific (a) file(s). For a respective file identifier, "ham"
         for example, to be collected, the keyword value may be entered
         as:
 
-        --fileid=ham or -fileid=ham
+        --fileid ham or -fileid ham
 
-        For multiple file identifiers to be collect, 'ham' and 'eggs'
+        For multiple file identifiers to be collect, "ham" and "eggs"
         in this example, the keyword value may be entered as (no
         spaces between comma-delimited values):
 
-        --fileid=ham,eggs or -fileid=ham,eggs
+        --fileid ham,eggs or -fileid ham,eggs
+
+
+Author(s)
+---------
+
+    Henry R. Winterbottom; 12 December 2022
+
+History
+-------
+
+    2022-12-12: Henry Winterbottom -- Initial implementation.
+
+"""
+
+# ----
+
+import os
+import time
+
+from schema import Optional, Or
+from staging.fetch import Fetch
+from utils.arguments_interface import Arguments
+from utils.logger_interface import Logger
+
+# ----
+
+__author__ = "Henry R. Winterbottom"
+__maintainer__ = "Henry R. Winterbottom"
+__email__ = "henry.winterbottom@noaa.gov"
+
+# ----
+
+# Specify whether to evaluate the format for the respective parameter
+# values.
+EVAL_SCHEMA = True
+
+# Define the schema attributes.
+cls_schema = {
+    "yaml_file": str,
+    "cycle": Or(str, int),
+    "work_path": str,
+    "expt_name": str,
+    Optional("fetch_type"): Or(str, None),
+    Optional("platform"): Or(str, None),
+    Optional("fileid"): Or(str, None),
+}
+
+# ----
+
+
+def main() -> None:
+    """
+    Description
+    -----------
+
+    This is the driver-level function to invoke the tasks within this
+    script.
 
     """
-
-    # Define the schema attributes.
-    cls_schema = {
-        "yaml_file": str,
-        "cycle": Or(str, int),
-        "work_path": str,
-        "expt_name": str,
-        Optional("fetch_type"): str,
-        Optional("platform"): str,
-        Optional("fileid"): str,
-    }
 
     # Collect the command line arguments.
     script_name = os.path.basename(__file__)
     start_time = time.time()
     msg = f"Beginning application {script_name}."
     Logger().info(msg=msg)
-    options_obj = Arguments().run(eval_schema=True, cls_schema=cls_schema)
+    options_obj = Arguments().run(eval_schema=EVAL_SCHEMA, cls_schema=cls_schema)
 
     # Launch the task.
     task = Fetch(options_obj=options_obj)
