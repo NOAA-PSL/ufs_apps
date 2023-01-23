7e3fe1b012e2d2bc40affd2801fe20d7d359757c
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-01-23 16:41:36 -0700

    Adding place holder file for LETKF module.

---
+++ ush/gdas/soca/letkf.py
@@ -0,0 +1,13 @@
+from gdas.soca import SOCA
+
+
+class GlobalLETKF():
+
+    def __init__(self, options_obj: object):
+        """ """
+
+    def run(self):
+        """
+
+        """
+        print("hello from SOCA letkf.")
